70b6aacc71954077da47b53fab86053bf1e64ea3
sanemat <o.gata.ken@gmail.com>
2010-06-21 00:36:59 +0900

    add watchr script

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 group :runtime do
   gem 'watchr'
+#  gem 'screenout', :git => 'git://github.com/sanemat/screenout.git', :ref => '128ff6'
 end
 
 group :test do
---
+++ bin/symfony_watchr.rb
@@ -0,0 +1,52 @@
+# Run me with:
+#
+#   $ watchr bin/symfony_watchr.rb
+
+# --------------------------------------------------
+# Convenience Methods
+# --------------------------------------------------
+#require 'rubygems'
+#require 'bundler'
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'screenout'))
+#Bundler.setup(:runtime)
+#require 'screenout'
+def run(cmd)
+  puts(cmd)
+  str = `#{cmd}`
+  puts(str)
+  message = parse(str)
+  Screenout::message(message[:message], message[:color])
+end
+
+def parse(string)
+  if /All tests successful/ =~ string
+    {:message => 'All Green', :color => :green}
+  else
+    {:message => 'Failed', :color => :red}
+  end
+end
+def run_all_tests
+  cmd = "php symfony test:unit"
+  run(cmd)
+end
+
+# --------------------------------------------------
+# Watchr Rules
+# --------------------------------------------------
+watch ( '^test.*/unit/.*\.php' ) {|m| run_all_tests }
+#watch( '^test.*/test_.*\.rb'   )   { |m| run( "ruby -rubygems %s"              % m[0] ) }
+#watch( '^lib/(.*)\.rb'         )   { |m| run( "ruby -rubygems test/test_%s.rb" % m[1] ) }
+#watch( '^lib/.*/(.*)\.rb'      )   { |m| run( "ruby -rubygems test/test_%s.rb" % m[1] ) }
+#watch( '^test/test_helper\.rb' )   { run_all_tests }
+
+# --------------------------------------------------
+# Signal Handling
+# --------------------------------------------------
+# Ctrl-\
+Signal.trap('QUIT') do
+  puts " --- Running all tests ---\n\n"
+  run_all_tests
+end
+
+# Ctrl-C
+Signal.trap('INT') { abort("\n") }
